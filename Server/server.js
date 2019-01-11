a5bccb8b38055927f3c79e4cd777d43330ac31fe
unknown <saishrege24@gmail.com>
2019-01-11 11:42:51 +0530

    MongoDb

--- Server/server.js
+++ Server/server.js
@@ -2,17 +2,29 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var User = mongoose.model('User',{
+var User = mongoose.model('users',{
     Name:{
-        type: String
+        type: String,
+        trim: true
     },
     Age:{
         type: Number
+    },
+    Email:{
+        type: String,
+        minlength: 1,
+        required: 2,
+        trim: true
     }
+
 });
  var newtd = new User({
-     Name: 'Saish'
- });
+     Name: 'Saish',
+     Age: 22,
+     Email: 'saishrege24@gmail.com',
+     
+ },
+ );
 
  newtd.save().then((doc)=>{
      console.log('Saved User',doc);
